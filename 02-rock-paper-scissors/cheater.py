3be71f474e6236854ebc25882288ed62032b0334
John Olsson <john@cryon.se>
2022-12-02 20:06:52 +0100

    Solutions for day 02

---
+++ 02-rock-paper-scissors/cheater.py
@@ -0,0 +1,36 @@
+
+scores = {
+    'A': 1, 'X': 1,  # rock / lose
+    'B': 2, 'Y': 2,  # paper / draw
+    'C': 3, 'Z': 3,  # scissors / win
+}
+
+
+def score_round_by_taking_given_move(opponent, me):
+    o_score, m_score = scores[opponent], scores[me]
+    outcome_score = (m_score - o_score + 1) % 3
+    return 3 * outcome_score + m_score
+
+
+def score_round_by_forcing_given_outcome(opponent, outcome):
+    o_score, outcome_score = scores[opponent], scores[outcome]
+    m_score = (o_score + outcome_score) % 3
+    return 3 * outcome_score + m_score - 2
+
+
+def solve():
+    part1 = sum([score_round_by_taking_given_move(*x) for x in parse_input("input")])
+    print(f"Score for part 1: {part1}")
+
+    part2 = sum(score_round_by_forcing_given_outcome(*x) for x in parse_input("input"))
+    print(f"Score for part 2: {part2}")
+
+
+def parse_input(path):
+    with open(path, "r") as input_file:
+        for line in input_file.readlines():
+            yield line.strip().split(" ")
+
+
+if __name__ == "__main__":
+    solve()
